dcc1519f09f423c7f2459c6e16bbb7fe16603c64
Sofija Simonovic <sofija.simonovic.2007@gmail.com>
2023-01-25 23:33:32 +0100

    upgraded JS files

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
+const playerConfigOverlay = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
+const cancelBtn = document.getElementById("cancel-btn");
 
+editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
-editPlayer2Btn.addEventListener("click", openPlayerConfig);
+
+cancelBtn.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
