5f2ee2797a92ab6a710b4e58ef4729c9d90c0c6d
Sofija Simonovic <sofija.simonovic.2007@gmail.com>
2023-01-31 21:18:05 +0100

    done

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,13 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
 
 const players = [
   {
@@ -11,21 +20,33 @@ const players = [
   },
 ];
 
-const playerConfigOverlay = document.getElementById("config-overlay");
+const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
-const errorsOutput = document.getElementById("config-errors");
-const gameArea = document.getElementById("active-game");
+const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
+const gameOverElement = document.getElementById("game-over");
 
-const editPlayer1Btn = document.getElementById("edit-player-1-btn");
-const editPlayer2Btn = document.getElementById("edit-player-2-btn");
-const cancelBtn = document.getElementById("cancel-btn");
-const startNewGameBtn = document.getElementById("start-game-btn");
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById("game-board");
 
-editPlayer1Btn.addEventListener("click", openPlayerConfig);
-editPlayer2Btn.addEventListener("click", openPlayerConfig);
+editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
-cancelBtn.addEventListener("click", closePlayerConfig);
+cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
 formElement.addEventListener("submit", savePlayerConfig);
-startNewGameBtn.addEventListener("click", startNewGame);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener('click', selectGameField);
+// }
+
+gameBoardElement.addEventListener("click", selectGameField);
--- scripts/config.js
+++ scripts/config.js
@@ -1,39 +1,37 @@
 function openPlayerConfig(event) {
-  editedPlayer = +event.target.dataset.playerid;
-  playerConfigOverlay.style.display = "block";
+  editedPlayer = +event.target.dataset.playerid; // +'1' => 1
+  playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
 
 function closePlayerConfig() {
-  playerConfigOverlay.style.display = "none";
+  playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
-  errorsOutput.textContent = "";
+  errorsOutputElement.textContent = "";
   formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get("playername").trim();
+  const enteredPlayername = formData.get("playername").trim(); // '      ' => ''
 
   if (!enteredPlayername) {
+    // enteredPlayername === ''
     event.target.firstElementChild.classList.add("error");
-    errorsOutput.textContent = "Enter a valid name!";
+    errorsOutputElement.textContent = "Please enter a valid name!";
     return;
   }
 
-  const updatedPlayerData = document.getElementById(
+  const updatedPlayerDataElement = document.getElementById(
     "player-" + editedPlayer + "-data"
   );
-  updatedPlayerData.children[1].textContent = enteredPlayername;
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
 
-  // if (editedPlayer === 1) {
-  //   players[0].name = enteredPlayername;
-  // } else {
-  //   players[1].name = enteredPlayername;
-  // }
+  players[editedPlayer - 1].name = enteredPlayername;
 
+  closePlayerConfig();
   players[editedPlayer - 1].name = enteredPlayername;
 
   closePlayerConfig();
